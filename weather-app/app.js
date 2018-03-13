82e04fd4e51512a1bd55089034cf93d50a783cd3
staxx6 <staxx@hotmail.de>
2018-03-13 14:03:56 +0100

    changed and removed api token for public

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
-
-const apiToken = 'AIzaSyCelFIBBOfYZW_44B9OT4yqnVHkv-Ud_tk';
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs.
   options({
@@ -16,21 +14,10 @@ const argv = yargs.
 .alias('help', 'h')
 .argv;
 
-let userAddressURI = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddressURI}&key=${apiToken}`,
-  json: true
-}, (error, response, body) => {
-  if(error) {
-    console.log('Unable to connect to Google servers.');
-  } else if(body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if(body.status === 'OK') {
-    console.log(`Adress: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`)
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`)
-  } else {
-    console.log('Sorry something is wrong here');
-  }
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
 });
